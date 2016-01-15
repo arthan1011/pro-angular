87cccda1dad4869e8fa78ea72608cc7843f061eb
Artur Shamsiev <arthan1011@gmail.com>
2016-01-15 15:19:57 +0300

    ch1 behavior

--- todo/todoApp.js
+++ todo/todoApp.js
@@ -15,4 +15,21 @@ var model = {
 var todoApp = angular.module("todoApp", []);
 todoApp.controller("ToDoCtrl", function($scope) {
     $scope.todo = model;
+
+    $scope.incompleteCount = function() {
+        var count = 0;
+
+        angular.forEach($scope.todo.items, function(item) {
+            if (!item.done) {count++}
+        });
+        return count;
+    };
+
+    $scope.warningLevel = function() {
+        return $scope.incompleteCount() > 1 ? ($scope.incompleteCount() > 2 ? 'label-danger' : 'label-warning') : 'label-success';
+    };
+
+    $scope.addNewItem = function(actionText) {
+        $scope.todo.items.push({action: actionText, done: false});
+    };
 });
