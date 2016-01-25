98456cb8165a2831558abaf8828a89a6b4e64bf6
Artur Shamsiev <arthan1011@gmail.com>
2016-01-25 12:13:40 +0300

    sports: url navigation

--- sports/app.js
+++ sports/app.js
@@ -2,4 +2,18 @@
  * Created by ashamsiev on 20.01.2016.
  */
 
-angular.module("sportsStore", ["customFilters", "cart"]);
+angular.module("sportsStore", ["customFilters", "cart", "ngRoute"])
+    .config(function($routeProvider) {
+
+        $routeProvider.when('/checkout', {
+            templateUrl: '/sports/views/checkoutSummary.html'
+        });
+
+        $routeProvider.when('/products', {
+            templateUrl: '/sports/views/productList.html'
+        });
+
+        $routeProvider.otherwise({
+            templateUrl: '/sports/views/productList.html'
+        });
+    });
