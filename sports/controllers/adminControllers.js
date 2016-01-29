a1999403d1f4c79597f2f79bc6f1ed16d3e1d2d8
Artur Shamsiev <arthan1011@gmail.com>
2016-01-29 17:13:42 +0300

    sports: admin console orders

--- sports/controllers/adminControllers.js
+++ sports/controllers/adminControllers.js
@@ -4,6 +4,7 @@
 
 angular.module('sportsStoreAdmin')
 .constant('authUrl', 'http://localhost:5500/users/login')
+.constant('ordersUrl', 'http://localhost:5500/orders')
     .controller('authCtrl', function($scope, $http, $location, authUrl) {
 
         $scope.authenticate = function (user, pass) {
@@ -31,4 +32,30 @@ angular.module('sportsStoreAdmin')
         $scope.getScreen = function() {
             return $scope.current == 'Products' ? '/sports/views/adminProducts.html' : '/sports/views/adminOrders.html';
         };
+    })
+    .controller('ordersCtrl', function($scope, $http, ordersUrl) {
+        $http.get(ordersUrl, {
+            withCredentials: true
+        })
+            .success(function(data) {
+                $scope.orders = data;
+            })
+            .error(function(error) {
+                $scope.error = error;
+            });
+
+        $scope.selectedOrder;
+
+        $scope.selectOrder = function(order) {
+            $scope.selectedOrder = order;
+        };
+
+        $scope.calcTotal = function(order) {
+            var total = 0;
+            for (var i = 0; i < order.products.length; i++) {
+                var product = order.products[i];
+                total += product.count * product.price;
+            }
+            return total;
+        }
     });
