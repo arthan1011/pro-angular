aee44ee9efcdafc796f0e27176418b634bdbe2ad
arthan1011 <arthan1011@gmail.com>
2016-01-30 15:45:44 +0300

    sports: admin console done

--- sports/admin.js
+++ sports/admin.js
@@ -2,7 +2,7 @@
  * Created by ashamsiev on 29.01.2016.
  */
 
-angular.module('sportsStoreAdmin', ['ngRoute'])
+angular.module('sportsStoreAdmin', ['ngRoute', 'ngResource'])
     .config(function($routeProvider) {
         $routeProvider.when('/login', {
             templateUrl: 'views/adminLogin.html'
