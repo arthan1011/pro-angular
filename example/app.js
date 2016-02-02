d9c8a912232cebafc33bff65677038056266d525
Artur Shamsiev <arthan1011@gmail.com>
2016-02-02 16:31:49 +0300

    example: directive

--- example/app.js
+++ example/app.js
@@ -10,4 +10,11 @@ angular.module('exampleApp', [])
     .controller('tomorrowCtrl', function($scope) {
         var dayNames = [ 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Saturday' ];
         $scope.day = dayNames[((new Date).getDay() + 1) % 7]
+    })
+    .directive('highlight', function() {
+        return function (scope, element, attrs) {
+            if (scope.day == attrs['highlight']) {
+                element.css('color', 'red');
+            }
+        }
     });
