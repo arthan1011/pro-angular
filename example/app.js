f2ce3d3c157c70e000050af848661cc7aaf9fd28
Artur Shamsiev <arthan1011@gmail.com>
2016-02-03 15:15:17 +0300

    example: service

--- example/app.js
+++ example/app.js
@@ -3,11 +3,14 @@
  */
 
 angular.module('exampleApp', [])
-    .controller('dayCtrl', function ($scope) {
-        $scope.day = (new Date).getDay();
+    .value('nowValue', new Date())
+    .controller('dayCtrl', function ($scope, days) {
+        //$scope.day = (new Date).getDay();
+        $scope.day = days.today;
     })
-    .controller('tomorrowCtrl', function ($scope) {
-        $scope.day = (new Date).getDay() + 1;
+    .controller('tomorrowCtrl', function ($scope, days) {
+        //$scope.day = (new Date).getDay() + 1;
+        $scope.day = days.tomorrow;
     })
     .directive('highlight', function ($filter) {
         var dayFilter = $filter('dayName');
@@ -23,4 +26,8 @@ angular.module('exampleApp', [])
         return function (input) {
             return angular.isNumber(input) ? dayNames[input] : input;
         }
+    })
+    .service('days', function(nowValue) {
+        this.today = nowValue.getDay();
+        this.tomorrow = (this.today + 1) % 7;
     });
